08bca44bbe5e54118eab7df412291ef964b70d9e
Griffen <gedge10@gmail.com>
2018-04-22 21:01:49 +1000

    Order of time
    - Slightly changed the order of projects to be more chronological
    - Updated the about copy

--- scripts/post.js
+++ scripts/post.js
@@ -1,8 +1,8 @@
 WebFont.load({
-	google: {
-		families: ['PT Sans:400,700,400italic,700italic']
-	}
+  google: {
+    families: ['PT Sans:400,700,400italic,700italic']
+  }
 });
-jQuery(document.links).filter(function() {
-	return this.hostname != window.location.hostname;
+jQuery(document.links).filter(function () {
+  return this.hostname != window.location.hostname;
 }).attr('target', '_blank');

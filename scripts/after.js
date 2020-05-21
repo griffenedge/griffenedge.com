fd7375a028124211da11f91c70f78d816e2544ae
Griffen <gedge10@gmail.com>
2020-05-21 15:41:18 +1000

    Minor design fixes
    - Properly calls Google Fonts for Aleo
    - Made links a little more accessible
    - Added padding
    - Removed unnecessary code
    - Removed CNAME file

--- scripts/after.js
+++ scripts/after.js
@@ -1,10 +1,8 @@
 WebFont.load({
   google: {
-    families: ['Lato:400;700,Aleo:700']
+    families: ['Lato:400;700', 'Aleo:700']
   }
 });
 jQuery(document.links).filter(function () {
   return this.hostname != window.location.hostname;
 }).attr('target', '_blank');
-
-const swup = new Swup();

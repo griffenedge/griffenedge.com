a4ae870c2420d3af64f96befe5ae7b91e2224afd
Griffen Edge <griffedge@users.noreply.github.com>
2020-12-15 16:26:17 +0000

    add: addRelNoOpener & addNewTabMessage
    Add rel="noopener" and new tab message to links with target="_blank"

--- scripts/after.js
+++ scripts/after.js
@@ -1,1 +1,18 @@
+function addRelNoOpener(link) {
+  let linkTypes = (link.getAttribute('rel') || '').split(' ');
+  if (!linkTypes.includes('noopener')) {
+    linkTypes.push('noopener');
   }
+  link.setAttribute('rel', linkTypes.join(' ').trim());
+}
+
+function addNewTabMessage(link) {
+  if (!link.querySelector('.screen-reader-only')) {
+    link.insertAdjacentHTML('beforeend', '<span class="sr-only">(opens in a new tab)</span>');
+  }
+}
+
+document.querySelectorAll('a[target="_blank"]').forEach(link => {
+  addRelNoOpener(link);
+  addNewTabMessage(link);
+});

c2d07d9100a634b4c10e3be2ec28e9f5a348a649
Griffen Edge <11290702+griffenedge@users.noreply.github.com>
2021-01-15 19:01:19 +0000

    move input to src
    - move input files to src directory
    - move all images, fonts and pdf to assets directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
 module.exports = function (eleventyConfig) {
 
+  // Specify files or directories for Eleventy to copy to output.
+  eleventyConfig.addPassthroughCopy("src/style");
+  eleventyConfig.addPassthroughCopy("src/script");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
   // Return Config object
   return {
     dir: {
+      input: "src",
       output: "dist"
     }
   };
---
+++ src/script/after.js
@@ -0,0 +1,40 @@
+/** Script that is placed at the end of the body */
+
+function addRelNoOpener(link) {
+  let linkTypes = (link.getAttribute('rel') || '').split(' ');
+  if (!linkTypes.includes('noopener')) {
+    linkTypes.push('noopener');
+  }
+  link.setAttribute('rel', linkTypes.join(' ').trim());
+}
+
+function addNewTabMessage(link) {
+  if (!link.querySelector('.visually-hidden')) {
+    link.insertAdjacentHTML('beforeend', '<span class="visually-hidden"> (opens in a new tab)</span>');
+  }
+}
+
+document.querySelectorAll('a[target="_blank"]').forEach(link => {
+  addRelNoOpener(link);
+  addNewTabMessage(link);
+});
+
+if ("fonts" in document) {
+  var latoRegular = new FontFace("Lato", "url(/assets/fonts/lato/Lato-Regular-hint-all.woff2) format('woff2'), url(/assets/fonts/lato/Lato-Regular-hint-all.woff) format('woff')");
+  var latoBold = new FontFace("Lato", "url(/assets/fonts/lato/Lato-Bold-hint-all.woff2) format('woff2'), url(/assets/fonts/lato/Lato-Bold-hint-all.woff) format('woff')", { weight: "700" });
+  var aleoBold = new FontFace("Aleo", "url(/assets/fonts/aleo/Aleo-Bold-hint-all.woff2) format('woff2'), url(/assets/fonts/aleo/Aleo-Bold-hint-all.woff) format('woff')", { weight: "700" });
+
+  Promise.all([latoRegular.load(), latoBold.load(), aleoBold.load()]).then(function (fonts) {
+    fonts.forEach(function (font) {
+      document.fonts.add(font);
+    });
+  });
+}
+
+if (!("fonts" in document) && "head" in document) {
+  // Awkwardly dump the second stage @font-face blocks in the head
+  var style = document.createElement("style");
+  // Note: Edge supports WOFF2
+  style.innerHTML = "@font-face { font-family: Lato; src: url(/assets/fonts/lato/Lato-Regular-hint-all.woff2) format('woff2'), url(/assets/fonts/lato/Lato-Regular-hint-all.woff) format('woff'); } @font-face { font-family: Lato; font-weight: 700; src: url(/assets/fonts/lato/Lato-Bold-hint-all.woff2) format('woff2'), url(/assets/fonts/lato/Lato-Bold-hint-all.woff) format('woff'); } @font-face { font-family: Aleo; font-weight: 700; src: url(/assets/fonts/aleo/Aleo-Bold-hint-all.woff2) format('woff2'), url(/assets/fonts/aleo/Aleo-Bold-hint-all.woff) format('woff'); }";
+  document.head.appendChild(style);
+}

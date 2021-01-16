b3d3449e1e72fa06144e87547857273d0ceeeeda
Griffen Edge <11290702+griffenedge@users.noreply.github.com>
2021-01-16 12:07:10 +0000

    eleventy sitemap
    Generate a sitemap using Eleventy, based on [eleventy-base-blog](https://github.com/11ty/eleventy-base-blog)

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const { DateTime } = require("luxon");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
+
+  // Plugins are custom code that Eleventy can import into a project from an external repository.
+  eleventyConfig.addPlugin(pluginRss);
 
   // Specify files or directories for Eleventy to copy to output.
   eleventyConfig.addPassthroughCopy("src/style");
@@ -7,6 +13,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site.webmanifest");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+  });
 
   // Return Config object
   return {

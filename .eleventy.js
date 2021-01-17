576106507ae3502a518a76dacfff1a6cbcfe40b1
Griffen Edge <11290702+griffenedge@users.noreply.github.com>
2021-01-17 21:03:27 +0000

    add eleventy 404.html
    based on [Adding a 404 Not Found Page to your Static Site](https://www.11ty.dev/docs/quicktips/not-found/)

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
@@ -17,6 +18,24 @@ module.exports = function (eleventyConfig) {
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+  });
+
+  // Browsersync Overrides
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, browserSync) {
+        browserSync.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('dist/404.html');
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+    ui: false,
+    ghostMode: false
   });
 
   // Return Config object
